f6c62230bf8cb32a0081576d3a227d8eb6751549
Brian <briankanya@gmail.com>
2021-09-16 02:44:51 -0400

    Added test for main PufferPanel class

--- tests/test_pulumi_gcp_pufferpanel.py
+++ tests/test_pulumi_gcp_pufferpanel.py
@@ -1,9 +1,11 @@
 """Tests for `pulumi_gcp_pufferpanel` module."""
 from typing import Generator
 
+import pulumi
 import pytest
 
 import pulumi_gcp_pufferpanel
+from pulumi_gcp_pufferpanel.pulumi_gcp_pufferpanel import PufferPanel
 
 
 @pytest.fixture
@@ -15,3 +17,42 @@ def version() -> Generator[str, None, None]:
 def test_version(version: str) -> None:
     """Sample pytest test function with the pytest fixture as an argument."""
     assert version == "0.4.1"
+
+
+class MyMocks(pulumi.runtime.Mocks):
+    """MyMocks."""
+
+    def call(self, args: pulumi.runtime.MockCallArgs) -> dict:  # type: ignore
+        """See https://www.pulumi.com/docs/reference/pkg/nodejs/pulumi/pulumi/runtime/#Mocks-call.
+
+        Args:
+            args (pulumi.runtime.MockCallArgs): Args for mock.
+
+        Returns:
+            dict: Needed provider implementation for this mock.
+        """
+        if args.token == "gcp:compute/getImage:getImage":
+            return {"archive_size_bytes": 1180189056}
+        return {}
+
+    def new_resource(self, args: pulumi.runtime.MockResourceArgs) -> list:  # type: ignore
+        """See https://www.pulumi.com/docs/reference/pkg/nodejs/pulumi/pulumi/runtime/#Mocks-newResource.
+
+        Args:
+            args (pulumi.runtime.MockResourceArgs): Args for mock.
+
+        Returns:
+            list: Unique identifier for this resource.
+        """
+        return [args.name + "_id", args.inputs]
+
+
+pulumi.runtime.set_mocks(MyMocks())
+
+
+@pulumi.runtime.test
+def test_create_pufferpanel_server() -> None:
+    """Test whether instantiating PufferPanel works."""
+    _ = PufferPanel(
+        name="PufferPanelServer", dns_name="bkanya.com", dns_zone="example-com-15fbd9a"
+    )
