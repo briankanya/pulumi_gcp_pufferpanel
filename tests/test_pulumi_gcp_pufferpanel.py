e6c41be55f02e26622e78c29d7437fe7823b5c18
Brian <briankanya@gmail.com>
2021-09-16 03:30:31 -0400

    Removed unnecessary test

--- tests/test_pulumi_gcp_pufferpanel.py
+++ tests/test_pulumi_gcp_pufferpanel.py
@@ -1,22 +1,7 @@
 """Tests for `pulumi_gcp_pufferpanel` module."""
-from typing import Generator
-
 import pulumi
-import pytest
 
-import pulumi_gcp_pufferpanel
 from pulumi_gcp_pufferpanel.pulumi_gcp_pufferpanel import PufferPanel
-
-
-@pytest.fixture
-def version() -> Generator[str, None, None]:
-    """Sample pytest fixture."""
-    yield pulumi_gcp_pufferpanel.__version__
-
-
-def test_version(version: str) -> None:
-    """Sample pytest test function with the pytest fixture as an argument."""
-    assert version == "0.4.2"
 
 
 class MyMocks(pulumi.runtime.Mocks):
