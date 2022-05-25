63379b2a2af8ec9db6450905373271f23a0f8b7f
Brian Kanya <briankanya@gmail.com>
2022-05-25 19:49:32 -0400

    Update
    - Ordered apt install
    - Switched to temurin deb repo
    - Installing temurin jdk 17 now

--- src/pulumi_gcp_pufferpanel/cloud_function/startup.sh
+++ src/pulumi_gcp_pufferpanel/cloud_function/startup.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 CODENAME=$(grep VERSION_CODENAME /etc/os-release | cut -d = -f 2)
-curl -Ls https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public > /tmp/public
-gpg --no-default-keyring --keyring /tmp/adoptopenjdk-keyring.gpg --import /tmp/public
-gpg --no-default-keyring --keyring /tmp/adoptopenjdk-keyring.gpg --export --output /usr/share/keyrings/adoptopenjdk-archive-keyring.gpg
-rm -f /tmp/public /tmp/adopt*
-echo "deb [signed-by=/usr/share/keyrings/adoptopenjdk-archive-keyring.gpg] https://adoptopenjdk.jfrog.io/adoptopenjdk/deb $CODENAME main" > /etc/apt/sources.list.d/adoptopenjdk.list
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -Ls https://packages.adoptium.net/artifactory/api/gpg/key/public > /usr/share/keyrings/adoptium.asc
+echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $CODENAME main" > /etc/apt/sources.list.d/adoptium.list
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt -y install adoptopenjdk-8-hotspot adoptopenjdk-11-hotspot adoptopenjdk-16-hotspot apt-transport-https ca-certificates curl gnupg lsb-release pufferpanel screen wget
+apt install -y \
+    ca-certificates \
+    gnupg \
+    lsb-release \
+    pufferpanel \
+    screen \
+    temurin-8-jdk \
+    temurin-11-jdk \
+    temurin-17-jdk \
+    wget
 apt -y upgrade
-systemctl enable docker
 systemctl enable pufferpanel
-systemctl restart docker
 systemctl restart pufferpanel
