44bb98e9882d10b9916a2b1198d8736221d7d0ab
Brian Kanya <briankanya@gmail.com>
2022-05-25 19:17:26 -0400

    Added jdk 11

--- src/pulumi_gcp_pufferpanel/cloud_function/startup.sh
+++ src/pulumi_gcp_pufferpanel/cloud_function/startup.sh
@@ -8,7 +8,7 @@ echo "deb [signed-by=/usr/share/keyrings/adoptopenjdk-archive-keyring.gpg] https
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt -y install adoptopenjdk-8-hotspot adoptopenjdk-16-hotspot apt-transport-https ca-certificates curl gnupg lsb-release pufferpanel screen wget
+apt -y install adoptopenjdk-8-hotspot adoptopenjdk-11-hotspot adoptopenjdk-16-hotspot apt-transport-https ca-certificates curl gnupg lsb-release pufferpanel screen wget
 apt -y upgrade
 systemctl enable docker
 systemctl enable pufferpanel
